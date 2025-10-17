6ab40e840b6eb14132d7ad8354829c54b2be56a3
Nandu <nandu.sharma@test.com>
2025-10-17 05:36:47 +0000

    add math functions

---
+++ src/main.php
@@ -0,0 +1,6 @@
+<?php 
+include 'math-utils.php';
+
+echo "Add 5 + 3 = " . add(5, 3) . "\n";
+echo "Subtract 5 - 5 = " . subtract(5, 3) . "\n";
+
---
+++ src/math-utils.php
@@ -0,0 +1,20 @@
+<?php
+function add($a, $b) {
+    return  $a + $b;
+}
+
+function subtract($a, $b) {
+    return $a - $b;
+}
+
+function multiple($a, $b) {
+    return $a * $b;
+}
+
+function divide($a, $b) {
+    if ($b ==0) return null;
+    return $a / $b;
+}
+
+
+

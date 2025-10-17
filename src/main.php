436cbbfa32a74d460d9340fe0bd9ec1e9211d28e
Nandu <nandu.sharma@test.com>
2025-10-17 05:52:51 +0000

    add string utils

--- src/main.php
+++ src/main.php
@@ -1,6 +1,10 @@
 <?php 
 include 'math-utils.php';
+include 'string.utils.php';
 
 echo "Add 5 + 3 = " . add(5, 3) . "\n";
 echo "Subtract 5 - 5 = " . subtract(5, 3) . "\n";
+
+echo"Uppercase 'helo' = " . uppercase('hello') . "\n";
+
 
---
+++ src/string-utils.php
@@ -0,0 +1,13 @@
+<?php
+
+function uppercase($str) {
+    return strtoupper($str);
+}
+
+function lowercase($str) {
+    return strtolower($str);
+}
+
+function capitalize($str) {
+    return ucfirst($str);
+}
